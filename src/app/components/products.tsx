2504f89d5b59bdf54ea826ad83a1bb05252c33ca
Rian <rianboleary@gmail.com>
2024-11-08 19:32:33 +0000

    Update products.tsx
    Fixed error when adding new Spaghetti item.

--- src/app/components/products.tsx
+++ src/app/components/products.tsx
@@ -9,13 +9,13 @@ export default function Products() {
       name: string;
   
   }
-    const [data, setData] = useState<any>([]);
+    const [data, setData] = useState<Item[]>([]);
   
     useEffect(() => {
       fetch('products.json')
         .then(response => response.json())
         .then(json => setData(json));
-    });
+    },[]);
   
     if (!data) {
       return <p>Loading...</p>;
@@ -29,7 +29,7 @@ export default function Products() {
           <li key={item.id}>{item.name}</li>
         ))}
       </ul>
-      <button onClick={()=> {const newdata =  data.concat({id:"5", name: "Spaghetti"}); setData(newdata)}} value="Spaghetti">
+      <button onClick={()=> {const newdata =  data.concat({id:data.length+1, name: "Spaghetti"}); setData(newdata)}} value="Spaghetti">
       Add Spaghetti
     </button>
     </div>      
